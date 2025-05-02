ac47f429c2fd7daa6d2d74e3c00fed37c0eb5bdf
maxJustiniano <maxjustiniano7@gmail.com>
2025-05-02 16:32:59 -0300

    Modo oscuro actualizado

--- darkMode.js
+++ darkMode.js
@@ -2,11 +2,13 @@ const darkModeButton = document.getElementById('dark-mode-toggle');
 const body = document.body;
 const navbar = document.querySelector('.navbar');
 const cards = document.querySelectorAll('.card');
+const titleCards = document.querySelectorAll('.card-title');
 
 darkModeButton.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
     navbar.classList.toggle('navbar-dark-mode');
     cards.forEach(card => card.classList.toggle('card-dark-mode'));
+    titleCards.forEach(card => card.classList.toggle('title-dark-mode'));
 
     if (body.classList.contains('dark-mode')) {
         darkModeButton.textContent = '🌑Modo Oscuro';
